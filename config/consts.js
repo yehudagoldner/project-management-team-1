d640ba4a57f2d75b37617c190d9d84f5d5c8e4f2
naamagi <n.gilat@gmail.com>
2022-07-26 10:25:18 +0300

    init server files

---
+++ config/consts.js
@@ -0,0 +1,7 @@
+const statuses = { 1: "TODO", 2: "IN_PROCESS", 3: "DONE" };
+const urgencies = { 1: "LOW", 2: "MEDIUM", 3: "HIGH" };
+
+
+
+
+module.exports = {statuses,urgencies};
---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+ require("./config/dbconnect");
+const app = express();
+
+app.set("view engine", "ejs");
+
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  console.log("Here");
+  res.render("index", { text: "world" });
+
+});
+
+const taskRouter = require("./routes/tasks");
+const projectRouter = require("./routes/projects");
+const subtaskRouter = require("./routes/subtasks");
+
+
+app.use("/tasks", taskRouter);
+app.use("/projects", projectRouter);
+app.use("/subtasks", subtaskRouter);
+
+function logger(req, res, next) {
+  console.log(req.originalUrl);
+  next();
+}
+app.listen(4000);
